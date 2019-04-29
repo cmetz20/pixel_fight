e11bb2e35019734e66ae074d90ea46953052f34f
cmetz20 <cmetz@email.arizona.edu>
2019-04-29 01:15:59 -0700

    Update pixelfight.js

--- pixelfight.js
+++ pixelfight.js
@@ -23,16 +23,16 @@ window.onload = function () {
     setInterval(gridChangeCheck, 500);
 }
 
-async function sendBoardRequest(first){
+async function sendBoardRequest(first) {
     try {
         const resp = await fetch("./pixelfight-api.php?board-request");
         var data = await resp.json();
         let buttons = document.getElementsByTagName("button");
-        for(let i = 0; i < data.length; i++){
-            if(buttons[i].className !== data[i]){
+        for (let i = 0; i < data.length; i++) {
+            if (buttons[i].className !== data[i]) {
                 buttons[i].className = data[i];
             }
-            if(first){
+            if (first) {
                 console.log("onclick set");
                 buttons[i].onclick = btnClick;
             }
@@ -47,10 +47,10 @@ async function sendBoardRequest(first){
 // to appropriately update database
 // function then reloads page to show updated DB
 async function btnClick(event) {
-    
-    // this.id = current btn and also point val in database
-    let message = this.id + getColorSelection();
-    const resp = await fetch("./pixelfight-api.php", {
+    try {
+        // this.id = current btn and also point val in database
+        let message = this.id + getColorSelection();
+        const resp = await fetch("./pixelfight-api.php", {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
@@ -58,12 +58,15 @@ async function btnClick(event) {
             },
             body: message
         });
-    var data = await resp.json();
-    console.log(data.body);
-    sendBoardRequest();
+        var data = await resp.json();
+        console.log(data.body);
+        sendBoardRequest();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-async function getIntitialUpdateID(){
+async function getIntitialUpdateID() {
     try {
         const resp = await fetch("./pixelfight-api.php?update-id");
         var data = await resp.json();
@@ -79,12 +82,12 @@ async function getIntitialUpdateID(){
 // for grid update. Only has to check an INT
 // rather than reupdating all the time for no reason.
 async function gridChangeCheck() {
-    if(currentUpdateID != -1){
+    if (currentUpdateID != -1) {
         try {
             const resp = await fetch("./pixelfight-api.php?update-id");
             var data = await resp.json();
-            if(data.type === "update-id"){
-                if(data.body !== currentUpdateID){
+            if (data.type === "update-id") {
+                if (data.body !== currentUpdateID) {
                     sendBoardRequest();
                 }
             }
