1a296e12f8013354fba8a0967ef870699755c80f
cmetz20 <cmetz@email.arizona.edu>
2019-04-29 00:48:57 -0700

    Update buttons_template.php

--- templates/buttons_template.php
+++ templates/buttons_template.php
@@ -1,110 +1,110 @@
-<button id="0-0"></button>
-<button id="0-1"></button>
-<button id="0-2"></button>
-<button id="0-3"></button>
-<button id="0-4"></button>
-<button id="0-5"></button>
-<button id="0-6"></button>
-<button id="0-7"></button>
-<button id="0-8"></button>
-<button id="0-9"></button>
+<button id="0-0" class="white"></button>
+<button id="0-1" class="white"></button>
+<button id="0-2" class="white"></button>
+<button id="0-3" class="white"></button>
+<button id="0-4" class="white"></button>
+<button id="0-5" class="white"></button>
+<button id="0-6" class="white"></button>
+<button id="0-7" class="white"></button>
+<button id="0-8" class="white"></button>
+<button id="0-9" class="white"></button>
 <br>
-<button id="1-0"></button>
-<button id="1-1"></button>
-<button id="1-2"></button>
-<button id="1-3"></button>
-<button id="1-4"></button>
-<button id="1-5"></button>
-<button id="1-6"></button>
-<button id="1-7"></button>
-<button id="1-8"></button>
-<button id="1-9"></button>
+<button id="1-0" class="white"></button>
+<button id="1-1" class="white"></button>
+<button id="1-2" class="white"></button>
+<button id="1-3" class="white"></button>
+<button id="1-4" class="white"></button>
+<button id="1-5" class="white"></button>
+<button id="1-6" class="white"></button>
+<button id="1-7" class="white"></button>
+<button id="1-8" class="white"></button>
+<button id="1-9" class="white"></button>
 <br>
-<button id="2-0"></button>
-<button id="2-1"></button>
-<button id="2-2"></button>
-<button id="2-3"></button>
-<button id="2-4"></button>
-<button id="2-5"></button>
-<button id="2-6"></button>
-<button id="2-7"></button>
-<button id="2-8"></button>
-<button id="2-9"></button>
+<button id="2-0" class="white"></button>
+<button id="2-1" class="white"></button>
+<button id="2-2" class="white"></button>
+<button id="2-3" class="white"></button>
+<button id="2-4" class="white"></button>
+<button id="2-5" class="white"></button>
+<button id="2-6" class="white"></button>
+<button id="2-7" class="white"></button>
+<button id="2-8" class="white"></button>
+<button id="2-9" class="white"></button>
 <br>
-<button id="3-0"></button>
-<button id="3-1"></button>
-<button id="3-2"></button>
-<button id="3-3"></button>
-<button id="3-4"></button>
-<button id="3-5"></button>
-<button id="3-6"></button>
-<button id="3-7"></button>
-<button id="3-8"></button>
-<button id="3-9"></button>
+<button id="3-0" class="white"></button>
+<button id="3-1" class="white"></button>
+<button id="3-2" class="white"></button>
+<button id="3-3" class="white"></button>
+<button id="3-4" class="white"></button>
+<button id="3-5" class="white"></button>
+<button id="3-6" class="white"></button>
+<button id="3-7" class="white"></button>
+<button id="3-8" class="white"></button>
+<button id="3-9" class="white"></button>
 <br>
-<button id="4-0"></button>
-<button id="4-1"></button>
-<button id="4-2"></button>
-<button id="4-3"></button>
-<button id="4-4"></button>
-<button id="4-5"></button>
-<button id="4-6"></button>
-<button id="4-7"></button>
-<button id="4-8"></button>
-<button id="4-9"></button>
+<button id="4-0" class="white"></button>
+<button id="4-1" class="white"></button>
+<button id="4-2" class="white"></button>
+<button id="4-3" class="white"></button>
+<button id="4-4" class="white"></button>
+<button id="4-5" class="white"></button>
+<button id="4-6" class="white"></button>
+<button id="4-7" class="white"></button>
+<button id="4-8" class="white"></button>
+<button id="4-9" class="white"></button>
 <br>
-<button id="5-0"></button>
-<button id="5-1"></button>
-<button id="5-2"></button>
-<button id="5-3"></button>
-<button id="5-4"></button>
-<button id="5-5"></button>
-<button id="5-6"></button>
-<button id="5-7"></button>
-<button id="5-8"></button>
-<button id="5-9"></button>
+<button id="5-0" class="white"></button>
+<button id="5-1" class="white"></button>
+<button id="5-2" class="white"></button>
+<button id="5-3" class="white"></button>
+<button id="5-4" class="white"></button>
+<button id="5-5" class="white"></button>
+<button id="5-6" class="white"></button>
+<button id="5-7" class="white"></button>
+<button id="5-8" class="white"></button>
+<button id="5-9" class="white"></button>
 <br>
-<button id="6-0"></button>
-<button id="6-1"></button>
-<button id="6-2"></button>
-<button id="6-3"></button>
-<button id="6-4"></button>
-<button id="6-5"></button>
-<button id="6-6"></button>
-<button id="6-7"></button>
-<button id="6-8"></button>
-<button id="6-9"></button>
+<button id="6-0" class="white"></button>
+<button id="6-1" class="white"></button>
+<button id="6-2" class="white"></button>
+<button id="6-3" class="white"></button>
+<button id="6-4" class="white"></button>
+<button id="6-5" class="white"></button>
+<button id="6-6" class="white"></button>
+<button id="6-7" class="white"></button>
+<button id="6-8" class="white"></button>
+<button id="6-9" class="white"></button>
 <br>
-<button id="7-0"></button>
-<button id="7-1"></button>
-<button id="7-2"></button>
-<button id="7-3"></button>
-<button id="7-4"></button>
-<button id="7-5"></button>
-<button id="7-6"></button>
-<button id="7-7"></button>
-<button id="7-8"></button>
-<button id="7-9"></button>
+<button id="7-0" class="white"></button>
+<button id="7-1" class="white"></button>
+<button id="7-2" class="white"></button>
+<button id="7-3" class="white"></button>
+<button id="7-4" class="white"></button>
+<button id="7-5" class="white"></button>
+<button id="7-6" class="white"></button>
+<button id="7-7" class="white"></button>
+<button id="7-8" class="white"></button>
+<button id="7-9" class="white"></button>
 <br>
-<button id="8-0"></button>
-<button id="8-1"></button>
-<button id="8-2"></button>
-<button id="8-3"></button>
-<button id="8-4"></button>
-<button id="8-5"></button>
-<button id="8-6"></button>
-<button id="8-7"></button>
-<button id="8-8"></button>
-<button id="8-9"></button>
+<button id="8-0" class="white"></button>
+<button id="8-1" class="white"></button>
+<button id="8-2" class="white"></button>
+<button id="8-3" class="white"></button>
+<button id="8-4" class="white"></button>
+<button id="8-5" class="white"></button>
+<button id="8-6" class="white"></button>
+<button id="8-7" class="white"></button>
+<button id="8-8" class="white"></button>
+<button id="8-9" class="white"></button>
 <br>
-<button id="9-0"></button>
-<button id="9-1"></button>
-<button id="9-2"></button>
-<button id="9-3"></button>
-<button id="9-4"></button>
-<button id="9-5"></button>
-<button id="9-6"></button>
-<button id="9-7"></button>
-<button id="9-8"></button>
-<button id="9-9"></button>
+<button id="9-0" class="white"></button>
+<button id="9-1" class="white"></button>
+<button id="9-2" class="white"></button>
+<button id="9-3" class="white"></button>
+<button id="9-4" class="white"></button>
+<button id="9-5" class="white"></button>
+<button id="9-6" class="white"></button>
+<button id="9-7" class="white"></button>
+<button id="9-8" class="white"></button>
+<button id="9-9" class="white"></button>
 <br>
