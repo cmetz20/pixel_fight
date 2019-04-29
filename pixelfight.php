6a4718282bd44a020a54b8c2b65ff2c6e52e42d5
cmetz20 <cmetz@email.arizona.edu>
2019-04-28 22:53:49 -0700

    Update pixelfight.php

--- pixelfight.php
+++ pixelfight.php
@@ -24,7 +24,7 @@ function updateButtonGrid(){
         while($row = mysqli_fetch_row($result)){
             // for each row in mysql table
             $count++;
-            $winning_index = 4;
+            $winning_index = 3;
             $winning_votes = $row[3];
             for($i = 2; $i < 13; $i++){
                 if($row[$i] > $winning_votes){
