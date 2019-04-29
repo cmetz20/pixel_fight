cf6eb955238ce6580599afd30f9c35dce6a5f88f
cmetz20 <cmetz@email.arizona.edu>
2019-04-28 23:11:15 -0700

    Update pixelfight.js

--- pixelfight.js
+++ pixelfight.js
@@ -56,7 +56,7 @@ function btnClick(event) {
 // rather than reupdating all the time for no reason.
 function gridChangeCheck() {
     let reload = false;
-    fetch("./pixelfight-API.php?update-id=" + currentUpdateID)
+    fetch("./pixelfight-API.php")
         .then(function (response) {
             console.log(response);
         });
