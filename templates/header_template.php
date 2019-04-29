e2bb75595ffea327b0ec37464b35798f4f2f75a9
cmetz20 <cmetz@email.arizona.edu>
2019-04-29 00:19:29 -0700

    Update header_template.php

--- templates/header_template.php
+++ templates/header_template.php
@@ -6,6 +6,6 @@
     </head>
     <body>
         <h1>Pixel Fight!</h1>
-        <h2>Board Version # <h2>
-        <p id="board-version"></p>
+        <h2>Board Version ID<h2>
+        <p id="board-version">#</p>
         
