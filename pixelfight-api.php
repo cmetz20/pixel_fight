14edf72cf31afc5a14ff299bd793f79e7bfa9de0
cmetz20 <cmetz@email.arizona.edu>
2019-04-28 23:32:17 -0700

    Update pixelfight-api.php

--- pixelfight-api.php
+++ pixelfight-api.php
@@ -1,5 +1,6 @@
 <?php
 $var =  "{type: update-id,";
 $var .=  "body: API reply}";
-echo json_encode($var);
+echo json_encode(array("type" => "update-id", 
+                       "body" => "API REPLY"));
 ?>
